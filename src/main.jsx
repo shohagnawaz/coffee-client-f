4c67eaeb6aa87c7c6ceeda6b10a6736c3a24e70e
shohagnawaz <snawazdigital@gmail.com>
2024-08-28 18:52:32 +0600

    signup

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import {
 } from "react-router-dom";
 import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +25,14 @@ const router = createBrowserRouter([
     path: "/updateCoffee/:id",
     element: <UpdateCoffee></UpdateCoffee>,
     loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
+  },
+  {
+    path: "/signIn",
+    element: <SignIn></SignIn>
+  },
+  {
+    path: "/signUp",
+    element: <SignUp></SignUp>
   }
 ]);
 
